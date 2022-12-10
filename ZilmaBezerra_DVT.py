31e6b351e4fde9f5d3a86ea4fde092162e876a51
Zilma Bezerra <zilmakarlla@gmail.com>
2022-12-10 11:35:45 +0000

    Update ZilmaBezerra_DVT.py

--- ZilmaBezerra_DVT.py
+++ ZilmaBezerra_DVT.py
@@ -22,7 +22,7 @@ lottie_book = load_lottieurl(file_url)
 st_lottie(lottie_book, speed = 1, height = 200, key = 'initial')
 
 st.markdown("<h1 style = 'text-align: center;'> Analysing Book Ratings Dataset </h1>", unsafe_allow_html = True)
-st.markdown("<h3 style = 'text-align: center;'> A Web App by [Zilma Bezerra](https://github.com/zilmabezerra) </h3>", unsafe_allow_html = True)
+st.markdown("<h3 style = 'text-align: center;'> A Web App by <b><a href = 'https://github.com/zilmabezerra'> Zilma Bezerra </a></b></h3>", unsafe_allow_html = True)
 #st.subheader('A Web App by [Zilma Bezerra](https://github.com/zilmabezerra)')
 
 '''
